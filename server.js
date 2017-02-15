d88253832f1e7cb5b07083c146cdd8be01bc088f
aidanbdh <aidanbdh1998@gmail.com>
2017-02-15 15:03:05 -0800

    Updated for 0.4.0 release

--- server.js
+++ server.js
@@ -9,33 +9,37 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     console.log('A user disconnected')
   })
-  socket.on('join', ({ name, file }) => {
+  socket.on('join', name => {
     socket.join(name)
     if(rooms.indexOf(name) === -1) {
       rooms.push(name)
-      rooms.push({ host: socket.id })
-      socket.emit('joined', name)
+      rooms.push({ host: socket.id, colors: ['white', 'black', 'maroon', 'red', 'pink', 'brown', 'orange', 'coral', 'olive', 'yellow', 'beige', 'lime', 'green', 'mint', 'teal', 'cyan', 'navy', 'blue', 'purple', 'lavender', 'magenta', 'grey'] })
+      console.log(`A user created ${name}`)
+      const num = Math.floor(Math.random() * rooms[rooms.indexOf(name) + 1].colors.length)
+      const color = rooms[rooms.indexOf(name) + 1].colors.slice(num, num + 1)
+      socket.emit('joined', { name, color})
     } else {
       socket.emit('file conflict')
     }
-    socket.on('continue', file => {
-      socket.broadcast.to(rooms[rooms.indexOf(name)+1].host).emit('get file', socket.id)
-      socket.on('send file', originalFile => {
-        if(originalFile === file) {
-          console.log(`A user joined ${name}`)
-        } else {
-          socket.emit('replace', originalFile)
-          socket.emit('joined', name)
-        }
-      })
+    socket.on('continue', () => {
+      socket.to(rooms[rooms.indexOf(name)+1].host).emit('get file', socket.id)
+    })
+    socket.on('send file', ({ id, file, cursors }) => {
+      console.log(`A user joined ${name}`)
+      const num = Math.floor(Math.random() * rooms[rooms.indexOf(name) + 1].colors.length)
+      const color = rooms[rooms.indexOf(name) + 1].colors.slice(num, num + 1)
+      socket.to(id).emit('replace', { file, newCursors: cursors, color })
+      socket.to(id).emit('joined', { name, color})
+    })
+    socket.on('new socket', (cursor) => {
+      socket.broadcast.to(name).emit('new socket', cursor)
+    })
+    socket.on('cursor moved', info => {
+      socket.broadcast.to(name).emit('update cursor', info)
     })
     socket.on('leave', function() { socket.leave(name) })
   })
-  socket.on('redirect', ({id, file}) => {
-    socket.broadcast.to(id).emit('redirect', file)
-  })
   socket.on('edit', event => {
-    console.log(event)
     socket.broadcast.to(event.name).emit('insert', event)
   })
 })
